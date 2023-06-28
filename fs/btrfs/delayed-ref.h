ea06fd230c0ea5df9d3ca029670cf3a98f6eb510
Boris Burkov <boris@bur.io>
2023-06-28 11:00:15 -0700

    btrfs: record simple quota deltas
    At the moment that we run delayed refs, we make the final ref-count
    based decision on creating/removing extent (and metadata) items.
    Therefore, it is exactly the spot to hook up simple quotas.
    
    There are a few important subtleties to the fields we must collect to
    accurately track simple quotas, particularly when removing an extent.
    When removing a data extent, the ref could be in any tree (due to
    reflink, for example) and so we need to recover the owning root id from
    the owner ref item. When removing a metadata extent, we know the owning
    root from the owner field in the header when we create the delayed ref,
    so we can recover it from there.
    
    We must also be careful to handle reservations properly to not leaked
    reserved space. The happy path is freeing the reservation when the
    simple quota delta runs on a data extent. If that doesn't happen, due to
    refs canceling out or some error, the ref head already has the
    must_insert_reserved machinery to handle this, so we piggy back on that
    and use it to clean up the reserved data.
    
    Signed-off-by: Boris Burkov <boris@bur.io>

--- fs/btrfs/delayed-ref.h
+++ fs/btrfs/delayed-ref.h
@@ -111,6 +111,12 @@ struct btrfs_delayed_ref_head {
 	int ref_mod;
 
 	/*
+	 * Track reserved bytes when setting must_insert_reserved.
+	 * On success or cleanup, we will need to free the reservation.
+	 */
+	u64 reserved_bytes;
+
+	/*
 	 * when a new extent is allocated, it is just reserved in memory
 	 * The actual extent isn't inserted into the extent allocation tree
 	 * until the delayed ref is processed.  must_insert_reserved is
